0401353771fd79e8ff6c438c3ae214451ef95068
Legorock <jasonjoe.sgia@gmail.com>
2026-01-29 21:56:17 +0700

    Revert "Merge branch 'Gill'"
    This reverts commit 1f84d8d5cf7c55b2ad96bc200c1ccf99dfae21f5, reversing
    changes made to e6a55530b4502d8c5d00e7342832d0e4d52c9661.

--- gimjam-26/player_projectile.gd
+++ gimjam-26/player_projectile.gd
@@ -1,17 +1,12 @@
 extends Area2D
 
 @onready var collision = $CollisionShape2D
-@onready var anim = $AnimatedSprite2D
-var speed = 1
-var lifetime = 1.0
-var direction: Vector2 = Vector2.RIGHT
-var fire_rate:float = 1
-#var anims = ["var1", "var2", "var3", "var_water"]
+@export var speed = 100
+
+var direction: Vector2 = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-
-	anim.play("var_water")
-	get_tree().create_timer(lifetime).timeout.connect(queue_free)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
